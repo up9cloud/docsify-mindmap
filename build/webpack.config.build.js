f1744b5e32eeecc5f1f16e626b65a0bdc5044b93
up9cloud <8325632+up9cloud@users.noreply.github.com>
2022-10-16 10:13:16 +0000

    up dev deps to date

--- build/webpack.config.build.js
+++ build/webpack.config.build.js
@@ -1,10 +1,12 @@
 const path = require('path')
 
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 
 const webpackBaseConfig = require('./webpack.config.base.js')
 
+const env = process.env.NODE_ENV || 'production'
 const webpackConfig = merge(webpackBaseConfig, {
+  mode: env,
   output: {
     path: path.resolve(__dirname, '..', 'dist'),
     filename: 'docsify-mindmap.min.js'
